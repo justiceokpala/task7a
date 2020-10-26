757d944956789b5feb5c1fa21bac8ad255657efc
Justice Okpala <justicecharles497@gmail.com>
2020-10-26 19:06:15 +0100

    first commit

---
+++ task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace taskk
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+ 
+
+            int[] a = new int[6] { 10, 20, 30, 40, 50, 20 };
+
+            int[] b = new int[6] { 5, 4, 3, 2, 1, 5 };
+
+            int[] arr = new int[6];
+
+            for (int i = 0, j = 0; i < a.Length; i++, j++)
+
+            {
+
+                arr[i] = a[i] + b[j];                             
+
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+
+                Console.Write(arr[i] + " ");
+
+            }
+
+            Console.Read();
+
+        }
+        }
+    }
